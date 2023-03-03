795a12149346bb9e2caf8bc7be4cf0e5f76d0499
Hamza Saqib <mianhamza7262@gmail.com>
2023-03-03 19:38:45 +0500

    new server ip added for api auth

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,192.81.216.213,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,192.81.216.213:80,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : ''
     ))),
 
